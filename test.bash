600848bae16371fe9e78a6c6b5f0807d1f530f2f
viktor.gvozdev <viktor.gvozdev@yessoftware.com>
2016-08-22 14:15:22 +0300

    под-скрипт пишем во временный файл

--- test.bash
+++ test.bash
@@ -1,33 +1,32 @@
 #!/usr/bin/bash
 export FILES_TO_DELETE="delete.txt"
-
+export TMP_FILE=`mktemp`
+printf "
+	export ORIGIN_TREE=\$(git ls-tree \$1)
+	export MODIFIED_TREE=\$(echo \"\$ORIGIN_TREE\" | grep -v -P \"(\$FILES_TO_DELETE)\")
+	if [[ \"\$#\" = \"1\" && \"\$MODIFIED_TREE\" != \"\" ]];
+		then
+			git commit-tree \`echo \"\$(printf \"\$MODIFIED_TREE\" | git mktree) \${@:2}\"\`
+			exit 0
+		else
+			for i in \`seq 3 2 \$#\`;
+				do
+					if [ \"\$(git ls-tree \${!i})\" != \"\$MODIFIED_TREE\" ];
+						then
+						git commit-tree \`echo \"\$(printf \"\$MODIFIED_TREE\" | git mktree) \${@:2}\"\`
+						exit
+					fi
+				done
+		  
+			for i in \`seq 3 2 \$#\`;
+				do
+					printf \"\${!i}\"
+				done
+	fi
+" > $TMP_FILE
 
 # TODO - дебаг
 git reset --hard __qwe
 
-git filter-branch -f --commit-filter '
-	printf "
-		export ORIGIN_TREE=\$(git ls-tree \$1)
-		export MODIFIED_TREE=\$(echo \"\$ORIGIN_TREE\" | grep -v -P \"(\$FILES_TO_DELETE)\")
-		if [[ \"\$#\" = \"1\" && \"\$MODIFIED_TREE\" != \"\" ]];
-			then
-				git commit-tree \`echo \"\$(printf \"\$MODIFIED_TREE\" | git mktree) \${@:2}\"\`
-				exit 0
-			else
-				for i in \`seq 3 2 \$#\`;
-					do
-						if [ \"\$(git ls-tree \${!i})\" != \"\$MODIFIED_TREE\" ];
-							then
-							git commit-tree \`echo \"\$(printf \"\$MODIFIED_TREE\" | git mktree) \${@:2}\"\`
-							exit
-						fi
-					done
-			  
-				for i in \`seq 3 2 \$#\`;
-					do
-						printf \"\${!i}\"
-					done
-		fi
-	" | bash /dev/stdin $@
-' HEAD
+git filter-branch -f --commit-filter 'bash $TMP_FILE $@' HEAD
 
