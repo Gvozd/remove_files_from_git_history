9a9604cbed47eef4d6614f916178620488c9a7a3
viktor.gvozdev <viktor.gvozdev@yessoftware.com>
2016-08-22 13:37:12 +0300

    * переформатировал табиками

--- test.bash
+++ test.bash
@@ -5,27 +5,26 @@ export FILES_TO_DELETE="delete.txt"
 git reset --hard __qwe
 
 git filter-branch -f --commit-filter '
-  export ORIGIN_TREE=$(git ls-tree $1)
-  export MODIFIED_TREE=$(echo "$ORIGIN_TREE" | grep -v -P "($FILES_TO_DELETE)")
-  if [[ "$#" = "1" && "$MODIFIED_TREE" != "" ]];
-    then
-      git commit-tree `echo "$(printf "$MODIFIED_TREE" | git mktree) ${@:2}"`
-	  exit 0
-	else
-      for i in `seq 3 2 $#`;
-      do
-        if [ "$(git ls-tree ${!i})" != "$MODIFIED_TREE" ];
-          then
+	export ORIGIN_TREE=$(git ls-tree $1)
+	export MODIFIED_TREE=$(echo "$ORIGIN_TREE" | grep -v -P "($FILES_TO_DELETE)")
+	if [[ "$#" = "1" && "$MODIFIED_TREE" != "" ]];
+		then
 			git commit-tree `echo "$(printf "$MODIFIED_TREE" | git mktree) ${@:2}"`
-			exit
-        fi
-      done
-	  
-      for i in `seq 3 2 $#`;
-      do
-		printf "${!i}"
-	  done
-      #git commit-tree `echo "$(printf "$MODIFIED_TREE" | git mktree) ${@:2}"`
-  fi
+			exit 0
+		else
+			for i in `seq 3 2 $#`;
+				do
+					if [ "$(git ls-tree ${!i})" != "$MODIFIED_TREE" ];
+						then
+						git commit-tree `echo "$(printf "$MODIFIED_TREE" | git mktree) ${@:2}"`
+						exit
+					fi
+				done
+		  
+			for i in `seq 3 2 $#`;
+				do
+					printf "${!i}"
+				done
+	fi
 ' HEAD
 
