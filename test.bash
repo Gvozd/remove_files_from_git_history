54e4beac8bdd6bb5301450c6a44a6e4f82958232
viktor.gvozdev <viktor.gvozdev@yessoftware.com>
2016-08-22 13:41:36 +0300

    * принудиловка выполнения в bash

--- test.bash
+++ test.bash
@@ -1,30 +1,33 @@
 #!/usr/bin/bash
 export FILES_TO_DELETE="delete.txt"
 
+
 # TODO - дебаг
 git reset --hard __qwe
 
 git filter-branch -f --commit-filter '
-	export ORIGIN_TREE=$(git ls-tree $1)
-	export MODIFIED_TREE=$(echo "$ORIGIN_TREE" | grep -v -P "($FILES_TO_DELETE)")
-	if [[ "$#" = "1" && "$MODIFIED_TREE" != "" ]];
-		then
-			git commit-tree `echo "$(printf "$MODIFIED_TREE" | git mktree) ${@:2}"`
-			exit 0
-		else
-			for i in `seq 3 2 $#`;
-				do
-					if [ "$(git ls-tree ${!i})" != "$MODIFIED_TREE" ];
-						then
-						git commit-tree `echo "$(printf "$MODIFIED_TREE" | git mktree) ${@:2}"`
-						exit
-					fi
-				done
-		  
-			for i in `seq 3 2 $#`;
-				do
-					printf "${!i}"
-				done
-	fi
+	echo "
+		export ORIGIN_TREE=$(git ls-tree $1)
+		export MODIFIED_TREE=$(echo \"$ORIGIN_TREE\" | grep -v -P \"($FILES_TO_DELETE)\")
+		if [[ \"$#\" = \"1\" && \"$MODIFIED_TREE\" != \"\" ]];
+			then
+				git commit-tree `echo \"$(printf \"$MODIFIED_TREE\" | git mktree) ${@:2}\"`
+				exit 0
+			else
+				for i in `seq 3 2 $#`;
+					do
+						if [ \"$(git ls-tree ${!i})\" != \"$MODIFIED_TREE\" ];
+							then
+							git commit-tree `echo \"$(printf \"$MODIFIED_TREE\" | git mktree) ${@:2}\"`
+							exit
+						fi
+					done
+			  
+				for i in `seq 3 2 $#`;
+					do
+						printf \"${!i}\"
+					done
+		fi
+	" | /usr/bin/bash
 ' HEAD
 
